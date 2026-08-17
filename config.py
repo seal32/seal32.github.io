ffcf6a0a6dfa65841dc95abb463c446ced73285b
seal32 <seal32@163.com>
2026-08-17 20:36:40 +1000

    update

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ ip_version_priority = "ipv4"
 source_urls = [
     "",
     "https://gh-proxy.com/raw.githubusercontent.com/alantang1977/iptv_api/refs/heads/main/output/live_ipv4.txt",
-    "http://iptv.4666888.xyz/FYTV.txt",
-    "https://gh-proxy.com/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg1",
     "https://gh-proxy.com/https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt",
+    "",
+    "",
     "",
 
 ]
